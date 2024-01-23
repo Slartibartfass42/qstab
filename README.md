34f8cb8377aaefdb2eea96b9e4bf1aeb90825376
Valerie Bettaque <v.l.bettaque@gmail.com>
2024-01-22 19:01:36 -0500

    Renamed syk.py to nora.py and added comments

--- README.md
+++ README.md
@@ -16,6 +16,6 @@ Newer versions might also work, but have not been tested.
 
 ## Usage
 
-The code relevant for the data generation in [arXiv:2303.16946](https://arxiv.org/abs/2303.16946) is primarily found in `syk.py`, whereas new random Cliffords/Weyls and stabilizers can be generally sampled using the code in `clifford.py` and `stabilizer.py` respectively. All important functions are commented indicating how to use them.
+The code relevant for the data generation in [arXiv:2303.16946](https://arxiv.org/abs/2303.16946) is primarily found in `nora.py`, whereas new random Cliffords/Weyls and stabilizers can be generally sampled using the code in `clifford.py` and `stabilizer.py` respectively. All important functions are commented indicating how to use them.
 
 Most functions also require a Galois field object (`GF`) as input parameter, which indicates the qudit dimension and can be generated using `galois.GF(p**m)` for some prime number `p > 2` and positive integer `m > 0`. For the paper `p = 3` and `m = 1` were used.
--- qstab/nora.py
+++ qstab/nora.py
@@ -7,23 +7,37 @@ import random as rand
 import qstab.symplectic as symp
 from qstab.clifford import Weyl, Clifford
 
-def generate_stepwise_k_scrambler(n, k, steps, GF):
+# Returns a "brickwall" Clifford operator consisting of D layers of n/k random
+# Cliffords acting on k randomly chosen sets of sites each. Requires that n is a
+# multiple of k.
+def generate_stepwise_k_scrambler(n, k, D, GF):
     scrambler = Clifford.random_k_site(n, k, GF)
-    for _ in range(steps - 1):
+    for _ in range(D - 1):
         single_step_scrambler = Clifford.random_k_site(n, k, GF)
         scrambler *= single_step_scrambler
     return scrambler
 
-def generate_syk_scrambler(n, n0, k, layers, steps_layer, GF):
+# Returns an array of Cliffords each encoding one "layer" of the NoRA tensor
+# network circuit. n is the total number of qudits/sites, n0 is the the number
+# of logical qudits, layers indicated the number of layers and D is the circuit
+# depth of the brickwall ciruit at each layer. k indicated the growth rate.
+def generate_nora_scrambler(n, n0, k, layers, D, GF):
     symps = [Clifford.identity(n, GF)] * layers
     for i in range(layers):
         n_scramble = n0 + k**(i+1)
         scrambler = generate_stepwise_k_scrambler \
-            (n_scramble, k, steps_layer, GF)
+            (n_scramble, k, D, GF)
         symp_layer = scrambler.embed(n, range(n_scramble))
         symps[i] = symp_layer
     return symps
 
+# Computes the code distance of a stabilizer code that was applied to one party
+# of a Bell stabilizer state (see bell_state in stabilizer.py) plus some number
+# n_aux of ancillary qudits. The layout of sites in bell_stab therefore has to
+# be of the form
+# [ <party_1> (n_party sites) | <clifford_code * (party_2 + ancilla)> (n_party + n_aux sites) ].
+# If n_mc is larger than 0 then a Monte Carlo method is used to approximate the
+# distance by limiting to the number of samples without progress to n_mc.
 def extract_code_distance(bell_stab, n_aux, n_mc=0):
     n_party = (bell_stab.n - n_aux) // 2
     sites_a = list(range(n_party))
@@ -52,7 +66,10 @@ def extract_code_distance(bell_stab, n_aux, n_mc=0):
             iter -= 1
         return d_max
 
-def syk_bell_scrambling(n0, k, layers, steps_layer, GF):
+# Applies all layers of the NoRA Clifford to a Bell stabilizer state with
+# n_party = n0 and k**layers ancilla sites. The resulting stabilizer sites are 
+# as required by extract_code_distance.
+def nora_bell_scrambling(n0, k, layers, D, GF):
     n_anc = k**layers
     n_syk = n0 + n_anc
     n_total = n_syk + n0
@@ -60,9 +77,9 @@ def syk_bell_scrambling(n0, k, layers, steps_layer, GF):
     bell_stab = Stabilizer.bell_state(n0, GF)
     anc_stab = Stabilizer.std(n_anc, n_anc, GF)
 
-    syk_stab = bell_stab + anc_stab\
+    syk_stab = bell_stab + anc_stab
 
-    symps = generate_syk_scrambler(n_syk, n0, k, layers, steps_layer, GF)
+    symps = generate_nora_scrambler(n_syk, n0, k, layers, D, GF)
 
     for s in symps:
         syk_stab = \
@@ -70,7 +87,10 @@ def syk_bell_scrambling(n0, k, layers, steps_layer, GF):
 
     return syk_stab
 
-def get_syk_weights(n0, k, layers, steps_layer, GF):
+# Returns the weight evolution of a stabilizer with initial minimal weight
+# at each layer of the NoRA tensor network.
+# For the parameters see generate_nora_scrambler.
+def get_syk_weights(n0, k, layers, D, GF):
     n = n0 + k**layers
     n1 = n - n0
     stab_0 = Stabilizer.std(n0, n0, GF)
@@ -80,7 +100,7 @@ def get_syk_weights(n0, k, layers, steps_layer, GF):
     weights = np.zeros((layers + 1, n))
     weights[0] = stab.get_weights()
 
-    symps = generate_syk_scrambler(n, n0, k, layers, steps_layer, GF)
+    symps = generate_nora_scrambler(n, n0, k, layers, D, GF)
     for i in range(layers):
         stab = stab.apply_clifford(symps[i])
         weights[i+1] = stab.get_weights()
